71034c2920d69412d99463d8557771b5b38a65db
mark0smith <24486728+mark0smith@users.noreply.github.com>
2021-10-01 10:25:55 +0800

    Update install_pip_for_python_2.7.sh

--- install_pip_for_python_2.7.sh
+++ install_pip_for_python_2.7.sh
@@ -12,15 +12,16 @@ cd ..
 
 
 unzip -q downloads/setuptools-44.1.1.zip
-python2.7 ./setuptools-44.1.1/setup.py install
+cd setuptools-44.1.1
+python2.7 bootstrap.py
+python2.7 setup.py install --user
+cd ..
 
 
 unzip -q downloads/pip-20.3.4-py2.py3-none-any.whl -d ./wheels
 cp downloads/pip-20.3.4-py2.py3-none-any.whl ./wheels
 
-python2.7 ./wheels/pip-20.3.4-py2.py3-none-any.whl/pip install --no-index --find-links ./wheels/ pip --ignore-installed
+python2.7 ./wheels/pip-20.3.4-py2.py3-none-any.whl/pip install --no-index --find-links ./wheels/ pip --ignore-installed --user
 
 
 python2.7 -m pip -V
-
-cd ..
